7e21de7b2e721a630184eaf2d85218aabbcda687
lilyayusupova <44448212+lilyayusupova@users.noreply.github.com>
2018-10-27 17:32:07 +0500

    Update FirstTest.java

--- Testname/src/test/java/firstTest/FirstTest.java
+++ Testname/src/test/java/firstTest/FirstTest.java
@@ -3,9 +3,7 @@ package firstTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,14 +11,14 @@ public class FirstTest {
     private static WebDriver driver;
 
     @BeforeClass
-    public static void main(String[] args) {
+    public void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Лилия\\Desktop\\Test\\chromedriver_win32");
         WebDriver driver = new Chromedriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://mail.ru/");
     }
-    @Test
+    @BeforeTest
     public void userLogin() {
         WebElement loginField = driver.findElement (By.xpath("//input[@id='mailbox:login']"));
         loginField.sendKeys("lilyayusupova5");
@@ -29,10 +27,31 @@ public class FirstTest {
         WebElement loginButton = driver.findElement(By.xpath(".//*[@id='mailbox__auth__button']"));
         loginButton.click();
     }
-    @AfterClass
-    public static void tearDown()  {
+    @Test (groups={"Y"})
+    public void Send(){
+        WebElement letterButton = driver.findElement(By.xpath("//*[@id='b-toolbar__left']"));
+        letterButton.click();
+        WebElement emailField = driver.findElement(By.xpath("//*[@id='toolkit-154063174678341composeForm']"));
+        emailField.sendKeys("lilyayusupova5@mail.ru");
+        WebElement textField = driver.findElement(By.xpath("//*[@id='tinymce']"));
+        textField.sendKeys("Hello,World!");
+        WebElement sendButton = driver.findElement(By.xpath("//*[@id='b-toolbar__right']"));
+        sendButton.click();
+        WebElement mailpage= driver.findElement(By.xpath("//*[@id='b-nav_folders'"));
+        mailpage.click();
+    }
+    public void Delete(){}
+    public void Language(){
+
+    }
+    @AfterGroups
+    public void userLogout (){
         WebElement logoutButton = driver.findElement(By.xpath("//*[@id='PH_logoutLink']"));
         logoutButton.click();
+    }
+    @AfterClass
+    public static void tearDown()  {
         driver.quit();
     }
 }
+
